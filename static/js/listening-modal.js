9c49617e4b63908c8f46f451ea45eed5feb63fae
NUS15305-11-dylwall <dylwall@nuevaschool.org>
2022-03-03 09:06:12 -0800

    New modal

--- static/js/listening-modal.js
+++ static/js/listening-modal.js
@@ -1,1 +1,15 @@
 
+const modal = document.querySelectorAll('.modal')[0];
+
+function close_modal() {
+	modal.classList.remove('is--visible');
+}
+
+function close_modal_and_start_game() {
+	modal.classList.remove('is--visible');
+	setTimeout(() => {setup_game();}, 1000);
+}
+window.onload = () => {
+	modal.classList.add('is--visible');
+}
+
