79128f5b7b9a93c9d8deac059262745ebf0f6077
dwLG00 <wallace.dylan00@gmail.com>
2022-04-30 01:30:08 -0700

    Added back faq

--- static/js/polyrhythm-modal.js
+++ static/js/polyrhythm-modal.js
@@ -1,11 +1,21 @@
 
 
-function close_modal() {
+function close_help_modal() {
 	let modal = document.getElementById('help-modal');
 	modal.classList.add('hidden');
+}
+
+function close_faq_modal() {
+	let modal = document.getElementById('faq-model');
+	model.classList.add('hidden');
 }
 
 function display_help_modal() {
 	let modal = document.getElementById('help-modal');
 	modal.classList.remove('hidden');
+}
+
+function display_faq_modal() {
+	let modal = document.getElementById('faq-modal');
+	model.classList.add('hidden');
 }
